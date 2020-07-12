337ac470364d62fa7e3e528d3f966c0af84732f2
Willy Stadnick <willy.stadnick@gmail.com>
2020-07-11 21:34:24 -0300

    Improve contact page layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,36 +8,46 @@
 </head>
 <body>
     <div class="container mt-3">
-        <h1>{{ env('APP_NAME') }}</h1>
-        @if (isset($alert))
-        <div class="alert alert-{{ $alert }}" role="alert">
-            {{ __($message) }}
+        <div class="row align-items-center justify-content-center">
+            <div class="col-md-6 align-self-center">
+                <div class="card">
+                    <div class="card-body">
+                        <h1>{{ env('APP_NAME') }}</h1>
+                        @if (isset($alert))
+                        <div class="alert alert-{{ $alert }}" role="alert">
+                            {{ __($message) }}
+                        </div>
+                        @endif
+                        <form method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="field-name">{{ __('app.form.name') }}</label>
+                                <input type="text" class="form-control" id="field-name" name="name" value="{{ old('name') }}" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="field-email">{{ __('app.form.email') }}</label>
+                                <input type="email" class="form-control" id="field-email" name="email" value="{{ old('email') }}" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="field-phone">{{ __('app.form.phone') }}</label>
+                                <input type="text" class="form-control" id="field-phone" name="phone" value="{{ old('phone') }}" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="field-message">{{ __('app.form.message') }}</label>
+                                <textarea class="form-control" id="field-message" name="message" required>{{ old('message') }}</textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="field-attach">{{ __('app.form.attach') }}</label>
+                                <input type="file" class="form-control-file" id="field-attach" name="attach" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary">{{ __('app.form.submit') }}</button>
+                            <input class="btn btn-secondary" type="reset" value="Reset">
+                            <a class="btn btn-danger" href="/" role="button">Cancel</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        @endif
-        <form method="post">
-            @csrf
-            <div class="form-group">
-                <label for="field-name">{{ __('app.form.name') }}</label>
-                <input type="text" class="form-control" id="field-name" name="name" value="{{ old('name') }}" required>
-            </div>
-            <div class="form-group">
-                <label for="field-email">{{ __('app.form.email') }}</label>
-                <input type="email" class="form-control" id="field-email" name="email" value="{{ old('email') }}" required>
-            </div>
-            <div class="form-group">
-                <label for="field-phone">{{ __('app.form.phone') }}</label>
-                <input type="text" class="form-control" id="field-phone" name="phone" value="{{ old('phone') }}" required>
-            </div>
-            <div class="form-group">
-                <label for="field-message">{{ __('app.form.message') }}</label>
-                <textarea class="form-control" id="field-message" name="message" required>{{ old('message') }}</textarea>
-            </div>
-            <div class="form-group">
-                <label for="field-attach">{{ __('app.form.attach') }}</label>
-                <input type="file" class="form-control-file" id="field-attach" name="attach" required>
-            </div>
-            <button type="submit" class="btn btn-primary">{{ __('app.form.submit') }}</button>
-        </form>
     </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
